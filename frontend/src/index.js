64e07c3ce12008f5a78eb5369380d3b3d45d6181
juliomchado <juliocarlos00@hotmail.com>
2020-04-09 20:54:45 +0100

    Local de produção criado

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Tarefas from './pages/Tarefas/Tarefas';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Tarefas />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
