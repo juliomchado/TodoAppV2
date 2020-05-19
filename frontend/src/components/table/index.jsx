5976529bb976ba1b8da70fa638d7e7a5de646574
juliomchado <juliocarlos00@hotmail.com>
2020-05-19 20:47:17 +0100

    Tabela separada em componente

---
+++ frontend/src/components/table/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import './index.css'
+
+export default props => {
+
+    return (
+        <table>
+            <thead>
+                <tr>
+
+                    <th>ID</th>
+                    <th>Tarefa</th>
+                    <th>Horario</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                </tr>
+                <tr>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                </tr>
+                <tr>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                </tr>
+                <tr>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                    <td>Sei la</td>
+                </tr>
+
+            </tbody>
+        </table>
+    )
+}
--- frontend/src/pages/Todo/index.jsx
+++ frontend/src/pages/Todo/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Container } from './styles.js'
 
+import Table from '../../components/table'
+
 export default props => {
 
 
@@ -11,44 +13,7 @@ export default props => {
             <div className="div-input">
                 <input type="text" placeholder="Pesquisar seu todo..." />
             </div>
-            <table>
-            <thead>
-                <tr>
-
-                    <th>ID</th>
-                    <th>Tarefa</th>
-                    <th>Horario</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                </tr>
-                <tr>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                </tr>
-                <tr>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                </tr>
-                <tr>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                    <td>Sei la</td>
-                </tr>
-
-            </tbody>
-        </table>
+           <Table/>
         </Container>
     )
 }
